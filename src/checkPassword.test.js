20a4f16b4412344707fb52b3af37633054f526dc
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2024-11-03 15:24:02 +0200

    tests for checkPassword were added

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,33 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('G0odP@assword')).toBe('boolean');
   });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {
-
+  it(`should return 'true' for the valid password`, () => {
+    expect(checkPassword('G0odP@assword')).toBe(true);
+    expect(checkPassword('!!!NewP@ass12')).toBe(true);
+    expect(checkPassword('Dog@00@@')).toBe(true);
+    expect(checkPassword('CaT10a0!>')).toBe(true);
+    expect(checkPassword('#%$*)6Gj')).toBe(true);
   });
 
-  // write more tests here
+  it(`should return 'false' for the invalid password`, () => {
+    expect(checkPassword('BadPassword')).toBe(false);
+    expect(checkPassword('DOGOG12321#')).toBe(false);
+    expect(checkPassword('12313kjs&&&')).toBe(false);
+    expect(checkPassword('#%$Я*)Gj')).toBe(false);
+  });
+
+  it(`should throw an error if password is not a string`, () => {
+    expect(() => checkPassword()).toThrow();
+    expect(() => checkPassword(null)).toThrow();
+    expect(() => checkPassword(false)).toThrow();
+    expect(() => checkPassword(true)).toThrow();
+    expect(() => checkPassword(NaN)).toThrow();
+    expect(() => checkPassword(9)).toThrow();
+    expect(() => checkPassword(0)).toThrow();
+    expect(() => checkPassword([])).toThrow();
+    expect(() => checkPassword(() => {})).toThrow();
+  });
 });
